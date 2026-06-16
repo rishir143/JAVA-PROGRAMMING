37c90d9180814d9d68172d750122e557ccf856ae
Rishi Choubey <shyamchoubey61@gmail.com>
2026-06-16 19:59:10 +0530

    Added DSA folder

---
+++ DSA/src/Heap/FindMedianInData.java
@@ -0,0 +1,31 @@
+package Heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class MedianFinder {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+        if(maxHeap.size() == 0) maxHeap.add(num);
+        else{
+            if(num < maxHeap.peek()) maxHeap.add(num);
+            else minHeap.add(num);
+        }
+        if(minHeap.size() == maxHeap.size() + 2) maxHeap.add(minHeap.remove());
+        if(maxHeap.size() == minHeap.size() + 2) minHeap.add(maxHeap.remove());
+    }
+
+    public double findMedian() {
+        if(minHeap.size() == maxHeap.size() + 1) return minHeap.peek();
+        else if(maxHeap.size() == minHeap.size() + 1) return maxHeap.peek();
+        else return (maxHeap.peek() + minHeap.peek())/2.0;
+    }
+}
+public class FindMedianInData
+
+}
