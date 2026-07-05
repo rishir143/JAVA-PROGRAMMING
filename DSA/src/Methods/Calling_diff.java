b90e3d2201ae0ffbf1e4c7fb5b4c1f1527b109ea
Rishi Choubey <shyamchoubey61@gmail.com>
2026-07-06 02:20:42 +0530

    Argument Q in method

---
+++ DSA/src/Methods/Calling_diff.java
@@ -0,0 +1,22 @@
+package Methods;
+
+public class Calling_diff {
+//    Prakher
+//    Praveen
+//    divya
+    public static void prakher(){
+        System.out.println("Prakher");
+        praveen();
+    }
+
+    public static void main(String[] args) {
+        divya();
+    }
+    public static void divya(){
+        prakher();
+        System.out.println("Divya");
+    }
+    public static void praveen(){
+        System.out.println("Praveen");
+    }
+}
