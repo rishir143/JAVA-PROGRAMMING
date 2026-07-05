8c2cd3b0c86accf6117d3e1c490b820ad19a8687
Rishi Choubey <shyamchoubey61@gmail.com>
2026-07-06 02:11:21 +0530

    Beginning of the methods

---
+++ DSA/src/Methods/BasicSyntax.java
@@ -0,0 +1,12 @@
+package Methods;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class BasicSyntax {
+    public static void harsh(){
+        System.out.println("Message from method");
+    }
+    public static void main(String[] args) {
+        harsh();
+    }
+}
