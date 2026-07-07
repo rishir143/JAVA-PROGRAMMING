485aaadcab32910977967a5f15f1cab94c4345b3
Rishi Choubey <shyamchoubey61@gmail.com>
2026-07-07 20:07:05 +0530

    Merge sorted array Q

---
+++ DSA/src/Array/MergeSortedArray.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 5};
+        int[] b = {2,5,6};
+        int[] c = new int[a.length + b.length];
+        int i = 0, j = 0, k =0;
+        while(i < a.length && j < b.length){
+            if(a[i] < b[j]) c[k++] = a[i++];
+            else c[k++] = b[j++];
+        }
+
+        while(i < a.length) c[k++] = a[i++];
+        while (j < b.length) c[k++] = b[j++];
+
+        System.out.println(Arrays.toString(c));
+    }
+}
