27f25f666ba34aaa6eac9f9dfa75f0be0759226b
Rishi Choubey <shyamchoubey61@gmail.com>
2026-07-10 01:07:31 +0530

    Transpose of the matrix

---
+++ DSA/src/Two_D_Array/Transpose_Matrix.java
@@ -0,0 +1,17 @@
+package Two_D_Array;
+
+public class Transpose_Matrix {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] nums = new int[m][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                nums[j][i] = matrix[i][j];
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
