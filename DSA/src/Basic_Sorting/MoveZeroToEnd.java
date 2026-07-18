dda76a66b1b877322e3f2a50e5f4b16c87f28e54
Rishi Choubey <shyamchoubey61@gmail.com>
2026-07-18 23:26:34 +0530

    Another method of Move zero to end

--- DSA/src/Basic_Sorting/MoveZeroToEnd.java
+++ DSA/src/Basic_Sorting/MoveZeroToEnd.java
@@ -17,4 +17,21 @@ public class MoveZeroToEnd {
             System.out.println(ele);
         }
     }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 6, 0, 4, 0, 8};
+        int n = arr.length;
+        int j = 0;
+        for (int i = 0; i < n  ; i++) {
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
 }
