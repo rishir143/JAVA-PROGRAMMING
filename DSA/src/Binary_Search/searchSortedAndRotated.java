7a745975b7bce6f82a368063573b106930d93137
Rishi Choubey <shyamchoubey61@gmail.com>
2026-07-26 00:08:07 +0530

    Q search in sorted and rotated array method - 2

--- DSA/src/Binary_Search/searchSortedAndRotated.java
+++ DSA/src/Binary_Search/searchSortedAndRotated.java
@@ -2,6 +2,22 @@ package Binary_Search;
 
 public class searchSortedAndRotated {
     public int search(int[] nums, int target) {
+    int lo = 0;
+    int hi = nums.length - 1;
 
+    while (lo <= hi){
+        int mid = lo + (hi - lo)/2;
+        if(nums[mid] == target) return  mid;
+        else if (nums[lo] <= nums[mid]) {
+
+            if(nums[lo] <= target && target < nums[mid]) hi = mid -1;
+            else lo = mid + 1;
+        }
+        else {
+            if(nums[mid] < target && target <= nums[hi]) lo = mid + 1;
+            else hi = mid - 1;
+        }
+    }
+    return -1;
     }
 }
