591eac8aa33b7b7c41107e07ddb132b534c43ad1
Rishi Choubey <shyamchoubey61@gmail.com>
2026-07-27 23:11:06 +0530

    StringBasics

---
+++ DSA/src/Recursion/StringBasics.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class StringBasics {
+    public static void main(String[] args) {\
+        String name = "Rishi";
+        System.out.println(change(name));
+    }
+
+     private static void change(String name){
+         name = "Rahul";
+     }
+}
