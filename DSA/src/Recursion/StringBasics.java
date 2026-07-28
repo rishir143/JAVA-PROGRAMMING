e3e0d870947dc6928709c716d8b1dd491286840f
Rishi Choubey <shyamchoubey61@gmail.com>
2026-07-28 23:03:13 +0530

    StringBasics

--- DSA/src/Recursion/StringBasics.java
+++ DSA/src/Recursion/StringBasics.java
@@ -1,12 +1,32 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringBasics {
-    public static void main(String[] args) {\
+    public static void main(String[] args) {
         String name = "Rishi";
-        System.out.println(change(name));
+        change(name);
+        System.out.println(name);
+        String[] names = {"Ayush", "Manoj", "Harish"};
+        System.out.println(Arrays.toString(names));
+        List<String> al = new ArrayList<>();
+        al.add("Rishi");
+        al.add("Raj");
+        al.add("Choubey");
+        al.add("Ayush");
+        al.add("Manoj");
+        System.out.println(al);
+        change2(al);
+        System.out.println(al);
+
     }
 
-     private static void change(String name){
-         name = "Rahul";
+     private static void change2(List<String> name){
+         name.add("Anish");
      }
+    private static void change(String name){
+        name = "hfhgfh";
+    }
 }
