36423979b161a50e469cba74ccb4c21ca9e1b9e1
Rishi Choubey <shyamchoubey61@gmail.com>
2026-08-05 21:17:38 +0530

    Did count and say Q

--- DSA/src/Recursion/LookAndSayPattern.java
+++ DSA/src/Recursion/LookAndSayPattern.java
@@ -1,5 +1,26 @@
 package Recursion;
 
+import java.util.Locale;
+
 public class LookAndSayPattern {
 
-}
+        public String countAndSay(int n) {
+            if(n == 1) return "1";
+           String prev = countAndSay(n - 1);
+           StringBuilder ans = new StringBuilder();
+           int count = 1;
+
+           for(int i = 1; i <= prev.length(); i++){
+               if(i < prev.length() && prev.charAt(i) == prev.charAt(i - 1)){
+                   count++;
+               }
+               else{
+                   ans.append(count);
+                   ans.append(prev.charAt(i - 1));
+                   count = 1;
+               }
+           }
+           return ans.toString();
+        }
+    }
+
