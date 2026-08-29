3a3f17be6599a864fe8d77936a4987f2a22e658a
Rishi Choubey <shyamchoubey61@gmail.com>
2026-08-30 02:21:40 +0530

    Inheritance

--- DSA/src/OOPS/Inheritance.java
+++ DSA/src/OOPS/Inheritance.java
@@ -1,5 +1,37 @@
 package OOPS;
+class Pokemon{
+    int power;
+    String type;
+    Pokemon(){
+
+    }
+    Pokemon(int power, String type){
+        this.power = power;
+        this.type = type;
+    }
+    void print(){
+        System.out.println(" Power " + this.power + " type " + type);
+    }
+}
+class StrongPokemon extends Pokemon{
+    int speed;
+}
+
+class LegendaryPokemon extends Pokemon{
+    String ability;
+}
+
+class GodPokemon extends LegendaryPokemon{
+    char tag;
+}
 
 public class Inheritance {
+    public static void main(String[] args) {
+        LegendaryPokemon mewtwo = new LegendaryPokemon();
+        mewtwo.ability = "Pressure";
+        mewtwo.print();
 
+        Pokemon pikachu = new Pokemon();
+        GodPokemon dialga = new GodPokemon();;
+    }
 }
